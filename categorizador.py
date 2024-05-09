b6ec34b5e25fba631a74db46d68e8c435b32c5a7
daniel.cibolli <daniel.cibolli@viasoft.com.br>
2024-05-09 08:15:22 -0300

    small changes while i learning

--- categorizador.py
+++ categorizador.py
@@ -4,23 +4,45 @@ import os
 
 load_dotenv()
 
-cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+def categoriza_produto(nome_produto, lista_categorias_possiveis):
+    prompt_sistema = f"""
+    Você é um categorizador de produtos.
+            Você deve assumir as categorias presentes na lista abaixo.
 
-resposta = cliente.chat.completions.create(
-    messages=[
-       {
-    "role":"system",
-    "content": """
-    Classifique o produto abaixo em uma das categorias: Higiene Pessoal, Moda ou Casa de uma descrição da categoria.
-    """
-},{
-    "role" : "user",
-    "content": """
-    Escova de dentes de bambu
-    """
-}
-    ],
-    model="gpt-4"
-)
+            # Lista de Categorias Válidas
+            {lista_categorias_possiveis.split(",")}
 
-print(resposta.choices[0].message.content)
+            # Formato da Saída
+            Produto: Nome do Produto
+            Categoria: apresente a categoria do produto
+
+            # Exemplo de Saída
+            Produto: Escova elétrica com recarga solar
+            Categoria: Eletrônicos Verdes
+    """
+    
+    cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    modelo = "gpt-4"
+
+    resposta = cliente.chat.completions.create(
+        messages=[
+        {
+        "role":"system",
+        "content": prompt_sistema
+        },{
+            "role" : "user",
+            "content": nome_produto
+        }
+        ],
+        model= modelo,
+        temperature=0,
+        max_tokens=200
+    )
+    return resposta.choices[0].message.content
+
+categorias_validas = input("informe as categorias validas seprando por virgula: ")
+
+while True: 
+    nome_produto = input("informe o nome do produto: ")
+    texto_resposta = categoriza_produto(nome_produto, categorias_validas)
+    print(texto_resposta)
